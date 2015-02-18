e1d685eac6dc55aed81ea9fa19b0976a877ddc80
Tim 'mithro' Ansell <mithro@mithis.com>
2015-02-18 15:20:21 +1100

    Making the script actually work.

--- .update.sh
+++ .update.sh
@@ -1,7 +1,16 @@
 #! /bin/bash
 
+set -e
+
 DATE=$(date +%Y%m%d-%H%M%S)
-SSH_WRAPPER=/tmp/getting-started/ssh
+
+export GIT_SSH=/tmp/getting-started-ssh
+# Tell ssh to use the given identity
+cat > $GIT_SSH <<'EOF'
+#!/bin/sh
+exec ssh -i /home/timvideos/.ssh/timvideos-website "$@"
+EOF
+chmod a+x $GIT_SSH
 
 cd /tmp
 
@@ -9,20 +18,12 @@ if [ -d getting-started ]; then
   rm -rf getting-started
 fi
 
-git clone git+ssh://github.com/timvideos/getting-started.git
-
-# Tell ssh to use the given identity
-cat > $SSH_WRAPPER <<'EOF'
-#!/bin/sh
-exec ssh -i /home/timvideos/.ssh/timvideos-website "$@"
-EOF
-chmod a+x $SSH_WRAPPER
-export GIT_SSH=$SSH_WRAPPER
+git clone git+ssh://git@github.com/timvideos/getting-started.git
 
 (
   cd getting-started
   echo $DATE > .keepalive
   git add .keepalive
   git commit -m "Daily commit on $DATE to keep getting-started at top of github."
-  git push master:keepalive --force
+  git push origin master:keepalive --force
 )
